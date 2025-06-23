f56a879a3ff8655f734f5d3810e0f43fe911f804
Joe Ham <joe@tulpa.ai>
2025-06-23 14:07:06 +0100

    Moved tests out to separate repo

--- actions/nessus/ImportNessusScanResultsFromCSV.py
+++ actions/nessus/ImportNessusScanResultsFromCSV.py
@@ -3,8 +3,9 @@ import pathlib
 from action_state_interface.action import Action
 from artefacts.ArtefactManager import ArtefactManager
 from Session import SessionManager
-from kg_api import Pattern
+from kg_api import Entity, Pattern, Relationship
 from action_state_interface.exec import ActionExecutionResult
+from kg_api.query import Query
 
 class ImportNessusScanResultsFromCSV(Action):
     """Import Nessus scan results from a CSV file."""
@@ -16,10 +17,18 @@ class ImportNessusScanResultsFromCSV(Action):
 
     def get_target_query(self):
         """Get the target query for the action."""
-        pass
-    
+        query = Query()
+        #Maybe throw in here a check to see if the Entity has the right file location Property before allowing the action?
+        query.match(Entity('NessusScanResultFile', alias='nessus_scan_result_file'))
+        query.ret_all()
+        return query
+
     def function(self, sessions: SessionManager, artefacts: ArtefactManager, pattern: Pattern):
         """Execute the action."""
+        # Read the location of the file from a property on the entity
+        # Read the file
+        # Parse the file
+        # Return the parsed data
         pass
     
     def capture_state_change(self, artefacts: ArtefactManager, pattern: Pattern, output: ActionExecutionResult):
