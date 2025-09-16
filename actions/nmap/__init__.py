a5739807f7076635cead5a51d7e7ab23dfb8b62c
Richard Balmer <richard.balmer@tulpa.ai>
2025-09-16 20:11:14 +0100

    Add new action, break out banner and vuln scripts into it

--- actions/nmap/__init__.py
+++ actions/nmap/__init__.py
@@ -1,6 +1,7 @@
 from .FastNmapScan import FastNmapScan
 from .SlowNmapScan import SlowNmapScan
 from .NmapAssetScan import NmapAssetScan
+from .NmapVulnerabilityScan import NmapVulnerabilityScan
 
 def get_actions() -> list:
-    return [FastNmapScan(), SlowNmapScan(), NmapAssetScan()]
+    return [FastNmapScan(), SlowNmapScan(), NmapAssetScan(), NmapVulnerabilityScan()]
