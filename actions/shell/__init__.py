e166b81076f6ebeddd49051481d67a2ef7a9d3e5
Jazib Khan <jazib@tulpa.ai>
2026-02-10 09:22:05 +0000

    remove database wipe import

--- actions/shell/__init__.py
+++ actions/shell/__init__.py
@@ -2,7 +2,6 @@ from .LocalRecursiveFileSearch import LocalRecursiveFileSearch
 from .IpRoute import IpRoute
 from .PasswordlessSudoCheck import PasswordlessSudoCheck
 from .ObtainShellOnExistingSession import ObtainShellOnExistingSession
-from .DatabaseWipe import DatabaseWipe
 
 
 def get_actions() -> list:
