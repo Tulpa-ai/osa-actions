b676e6010ae93c7c88d94912748a70070eae5b9a
Michalis Panayides <michalis.pan@tulpa.ai>
2026-02-20 11:09:22 +0000

    Remove ExtractCredentialsFromFile action

--- actions/shell/__init__.py
+++ actions/shell/__init__.py
@@ -2,8 +2,7 @@ from .LocalRecursiveFileSearch import LocalRecursiveFileSearch
 from .IpRoute import IpRoute
 from .PasswordlessSudoCheck import PasswordlessSudoCheck
 from .ObtainShellOnExistingSession import ObtainShellOnExistingSession
-from .ExtractCredentialsFromFile import ExtractCredentialsFromFile
 
 
 def get_actions() -> list:
-    return [LocalRecursiveFileSearch(), IpRoute(), PasswordlessSudoCheck(), ObtainShellOnExistingSession(), ExtractCredentialsFromFile()]
+    return [LocalRecursiveFileSearch(), IpRoute(), PasswordlessSudoCheck(), ObtainShellOnExistingSession()]
